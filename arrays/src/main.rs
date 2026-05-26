c73b4a245e59e09065f771e14eabbaacf959f2b6
Konataware <luansiloe404@gmail.com>
2026-05-26 17:10:02 -0300

    push

---
+++ arrays/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    
+    /*
+    * Arrays no Rust são de tamanho fixo
+    * e de tipo fixo
+    */
+    let arr = [1, 10, 5];
+
+    /*
+    * Esse array possui 5 ítens
+    */
+    let i_am_sized: [i32; 5] = [1, 2, 3, 4, 5];
+    
+    /*
+    * Esse array possui 5 ítens, todos são o valor 10
+    */
+    let all_ten: [10; 5];
+
+    /*
+    * Accessing arrays
+    */
+    let first = arr[0];
+    let last = arr[2];
+
+}
