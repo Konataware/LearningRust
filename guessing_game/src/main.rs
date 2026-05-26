5b3861a900b7b433c123e6b639e600e8b169eaf3
Konataware <luansiloe404@gmail.com>
2026-05-26 14:35:24 -0300

    chore

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,24 +4,28 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Adivinhe o número");
+    let secret_number: u8 = rand::thread_rng().gen_range(1..=100);
 
-    let secret_number: i8 = rand::thread_rng().gen_range(1..=100);
-    println!("O número secreto é {secret_number}");
+    println!("Adivinhe o número secreto.");
+    
+    loop {
+        let mut guess = String::new();
+        
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("ERRO: NÃO PUDE LER A LINHA.");
 
-    println!("Insira seu número");
+        let guess: u8 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue, // CATCH ALL xpr
+        };
 
-    let mut guess = String::new();
+        println!("You guessed: {guess}");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Muito baixo"),
-        Ordering::Greater => println!("Muito alto"),
-        Ordering::Equal => println!("GG"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Muito baixo"),
+            Ordering::Greater => println!("Muito alto"),
+            Ordering::Equal => println!("GG"),
+        }
     }
 }
